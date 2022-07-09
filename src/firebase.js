9759bc06d5fc977e6294e4f69977b75e0c8e53c4
Michelle <michey.asmah21@gmail.com>
2022-07-09 18:31:18 -0400

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/Storage";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUr_bMMbX1U2sRYktG5sLMO_IjLuugW0U",
+  authDomain: "what-was-said.firebaseapp.com",
+  projectId: "what-was-said",
+  storageBucket: "what-was-said.appspot.com",
+  messagingSenderId: "691674038815",
+  appId: "1:691674038815:web:41bf4ff5e02e331d048b2a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+ export const storage = getStorage(app);
+
