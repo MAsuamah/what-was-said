1620e7b346f141a50a74e652ef40f5f0632aed76
Michelle <michey.asmah21@gmail.com>
2022-07-10 20:24:44 -0400

    css styling

--- src/pages/Transcribe.js
+++ src/pages/Transcribe.js
@@ -60,47 +60,55 @@ function Transcribe() {
 
   const uploadAudio = () => {
     if(audioUpload === null) {
-      setErrMessage('Please select a File')
+      setErrMessage('Please Select a File!');
       return; 
     } 
 
     setIsLoading(true);
     setTranscript('');
-    setErrMessage('')
+    setErrMessage('');
 
-    const audioRef = ref(storage, `${audioUpload + v4()}`)
+    const audioRef = ref(storage, `${audioUpload + v4()}`);
+
     uploadBytes(audioRef, audioUpload).then(() => {
      getDownloadURL(audioRef).then((url) => {
-      generateTranscript(url)
+      generateTranscript(url);
      })
     }) 
   }
 
   return (
-    <Container>     
-      <Row> 
-        <Col lg={4} id="upload-intro">
-          <Form.Group controlId="formFileLg" className="mb-3">
-            <Form.Control type="file" size="lg" onChange={(event) => setAudioUpload(event.target.files[0])} />
-          </Form.Group>
-          <Button variant="primary" size="lg" onClick={uploadAudio} disabled={isLoading}>Get Transcript</Button>
-          {errMessage && (
-            <p>{errMessage}</p>
-          )}
-        </Col>
+    <div>
+      <Container>     
+        <Row className="row-style"> 
+          <Col lg={4} id="upload-intro" className="scroll">
+            <header><h1 className="title">Write-Out</h1></header>
+            <p id="para">
+              Transcribe your audio files with Write-out. <br/> Write-out uses the <a href="https://www.assemblyai.com/" target="_blank" rel="noreferrer">Assembly AI API</a> to quickly convert audio and video to text. Click <a href="https://www.assemblyai.com/docs#supported-file-types" target="_blank" rel="noreferrer">here</a> to see which files types are supported by Assembly AI.
+            </p>
+            <Form.Group controlId="formFileLg" className="mb-3">
+              <Form.Control type="file" size="lg" onChange={(event) => setAudioUpload(event.target.files[0])} />
+            </Form.Group>
+            <Button variant="primary" size="lg" onClick={uploadAudio} disabled={isLoading}>Get Transcript</Button>
+            {errMessage && (
+              <p id="err">{errMessage}</p>
+            )}
+          </Col>
 
-        <Col lg={8} >
-          {isLoading ? (
-            <div id="spinners" className="fetch">
-              <p>...Retrieving Transcript. This may take a few minutes...</p>
-              <Loader />
-            </div>
-          ) : 
-            <p className="fetch" id="transcript">{transcript}</p>
-          }
-        </Col>
-      </Row>
-    </Container>
+          <Col lg={8} className="scroll">
+            <h2 className="title">Your Transcript</h2>
+            {isLoading ? (
+              <div>
+                <p>...Retrieving Transcript. This may take a few minutes...</p>
+                <Loader />
+              </div>
+            ) : 
+              <p id={`${transcript && "transcript"}`}>{transcript}</p>
+            }
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
